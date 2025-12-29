a070a8650cfff526d465b89345de0ce0f89767e9
IVAN <ivan.ustimov.2012@mail.ru>
2025-12-29 13:47:12 +0300

    Кастомная команда load_test_data

--- catalog/management/commands/load_test_data.py
+++ catalog/management/commands/load_test_data.py
@@ -3,15 +3,13 @@ from django.core.management import call_command
 from catalog.models import Product, Category
 
 class Command(BaseCommand):
-    help = 'Очистка базы и загрузка тестовых данных из фикстур'
+    help = 'Очистка и загрузка тестовых данных из фикстур'
 
     def handle(self, *args, **kwargs):
-        # Удаляем все существующие данные
         Product.objects.all().delete()
         Category.objects.all().delete()
 
-        # Загружаем фикстуры
         call_command('loaddata', 'catalog/fixtures/categories.json')
         call_command('loaddata', 'catalog/fixtures/products.json')
 
-        self.stdout.write(self.style.SUCCESS('Тестовые данные успешно загружены из фикстур'))
+        self.stdout.write(self.style.SUCCESS('Данные загружены успешно!'))
