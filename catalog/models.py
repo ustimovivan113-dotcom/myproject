543ec4d8bc8cd6eaf16d9e95ee04ccaeb6eff645
IVAN <ivan.ustimov.2012@mail.ru>
2025-12-29 17:22:22 +0300

    Исправлен models.py — добавлены модели Product и Category с полями, Meta, __str__, ForeignKey

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
+    description = models.TextField(verbose_name='Описание', blank=True)
 
     def __str__(self):
         return self.name
@@ -12,9 +12,10 @@ class Category(models.Model):
         verbose_name_plural = 'категории'
         ordering = ['name']
 
+
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
+    description = models.TextField(verbose_name='Описание', blank=True)
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', related_name='products')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
